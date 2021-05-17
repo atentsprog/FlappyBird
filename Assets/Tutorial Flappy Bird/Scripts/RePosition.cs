c7bb89ae5271f2b5c24c11bf6f27ec2a3e4bd152
hktrollgames <hktrollgames@gmail.com>
2021-05-17 19:21:49 +0900

    기둥 잘 생성됨, 스크롤 잘됨, 재위치 잘 이동함

--- Assets/Tutorial Flappy Bird/Scripts/RePosition.cs
+++ Assets/Tutorial Flappy Bird/Scripts/RePosition.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class RePosition : MonoBehaviour
 {
-    public float minX = -30f; // 이값 보다 위치X가 작아지면 오른쪽으로 보내자.
+    public float minX;
+    public float width;
 
+    private void Start()
+    {
+        var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        width = spriteRenderer.sprite.bounds.size.x * transform.lossyScale.x;
+        //minX = -width;
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if(minX > transform.position.x )
+        // 최저 위치보다 뒤로 갔다면 앞으로 이동시키기
+        if (transform.position.x < minX)
         {
-            // 오른쪽으로 가로 크기 * 2만큼 보내자.
-            // 가로 크기 20.48
-            transform.Translate(20.48f * 2, 0, 0);
+            transform.Translate(width * 2, 0, 0);
         }
     }
 }
